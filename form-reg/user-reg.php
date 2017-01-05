e1419edae454076e7a4732e55fdc927e8813f1dd
Anna-Eliseeva <eliseeva9797@mail.ru>
2017-01-05 17:02:19 +0300

    форма регистрации.регистрирует и сохраняет пользователей

--- form-reg/user-reg.php
+++ form-reg/user-reg.php
@@ -5,7 +5,10 @@
  * Date: 05.01.2017
  * Time: 13:24
  */
-
+function passwordHash($pass){
+    $salt = 'hs5y44u1m5s15su35hy3s51str';
+    return md5($salt . $pass . $salt . 123457);
+}
 
 if(empty($_POST['userName'])){
     $error['userName'] = 'Введите имя!<br />';
@@ -19,8 +22,8 @@ if(empty($_POST['userPasswordCheck'])){
     $error['userPasswordCheck'] = 'Повторите пароль!<br />';
 };
 
-if($_POST['userPassword'] !== ['userPasswordCheck']){
-    $error['userName'] = 'Пароли не совпадают!<br />';
+if($_POST['userPassword'] !== $_POST['userPasswordCheck']){
+    $error['userPassword'] = 'Пароли не совпадают!<br />';
 };
 
 if(empty($_POST['userEmail'])){
@@ -28,7 +31,7 @@ if(empty($_POST['userEmail'])){
 };
 
 if(!filter_var($_POST['userEmail'], FILTER_VALIDATE_EMAIL)){
-    $error['userName'] = 'e-mail введен некорректно!<br />';
+    $error['userEmail'] = 'e-mail введен некорректно!<br />';
 };
 
 if(!empty($error)){
@@ -38,14 +41,29 @@ if(!empty($error)){
     exit;
 }
 
-print_r($_POST);
+$db = new PDO('mysql:dbname=form-registr;host=127.0.0.1', 'root', '');
+$sql = "
+INSERT INTO users
+SET
+  users.`name` = :userName,
+  users.`lastname` = :userLastName,
+  users.`age` = :userAge,
+  users.`sex` = :userSex,
+  users.`password_hash` = :userPasswordHash,
+  users.`e-mail` = :userEmail
+";
 
-/*
-currentDate: "05-01-2017 13:53:36"
-userLastName: "SHevchenko"
-userAge: "2020-02-02"
-userSex: "Male"
-userPassword: "pass"
-userPasswordCheck: "pass"
-userEmail
- */
+$statement = $db->prepare($sql);
+
+$statement->bindParam(':userName', $_POST['userName'], PDO::PARAM_STR);
+$statement->bindParam(':userLastName', $_POST['userLastName'], PDO::PARAM_STR);
+$statement->bindParam(':userAge', $_POST['userAge'], PDO::PARAM_STR);
+$statement->bindParam(':userSex', mb_strtolower($_POST['userSex']), PDO::PARAM_STR);
+$statement->bindParam(':userPasswordHash', passwordHash($_POST['userPassword']), PDO::PARAM_STR);
+$statement->bindParam(':userEmail', $_POST['userEmail'], PDO::PARAM_STR);
+
+$statement->execute();
+
+print_r($statement->errorInfo());
+
+print_r($_POST);
